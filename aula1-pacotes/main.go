98293b4b9622cd3ae44f56cdd4828f158f9396b6
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-11 16:47:06 -0300

    feat: instalando dependencias externas

--- aula1-pacotes/main.go
+++ aula1-pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import(
 	"modulo/auxiliar"
 	"fmt"
+	"github.com/badoux/checkmail"
 )
 
 func main(){
 	fmt.Println("Olá, mundo!")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("mariano.silva@gmail.com")
+	fmt.Println(erro)
 }
