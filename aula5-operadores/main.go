6097d714d7ef01e688099b8ecea3aa3ef2add32b
Mariano-SI <marianosilva.dev@gmail.com>
2025-02-12 11:28:22 -0300

    feat: operadores em Go

---
+++ aula5-operadores/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	//ARITMETICOS
+	//soma := 1 + 2
+	//subtracao := 1 - 2
+	//divisao := 1 / 2
+	//multiplicacao := 1 * 2
+	//restoDivisao := 1 % 2
+
+	//ATRIBUICAO
+	//var variavel1 string = "Variável 1"
+	//variavel2 := "Variável 2"
+
+	//COMPARACAO
+	//maior := 1 > 2
+	//menor := 1 < 2
+	//maiorOuIgual := 1 >= 2
+	//menorOuIgual := 1 <= 2
+	//igual := 1 == 2
+	//diferente := 1 != 2
+
+	//LOGICOS
+	//and := true && false
+	//or := true || false
+	//not := !true
+
+	//UNARIOS
+	//numero := 10
+	//numero++
+	//numero += 15
+	//numero--
+	//numero -= 20
+	//numero *= 3
+	//numero /= 2
+
+	//TERNARIO
+	// Em Go não existe operador ternário. Podemos usar if/else para simular o comportamento.
+
+
+
+
+}
