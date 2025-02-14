26be430e403c1f0ee7950069a766aa17945f978c
Mariano-SI <mariano.silva@ufvjm.edu.br>
2025-02-14 15:37:08 -0300

    feat: interfaces

---
+++ aula24-interfaces/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type retangulo struct {
+	altura float64
+	largura float64
+}
+
+func (r retangulo) area() float64 {
+	return r.altura * r.largura
+}
+
+type circulo struct{
+	raio float64
+}
+
+func (c circulo) area() float64 {
+	return c.raio * c.raio * 3.14
+}
+
+type forma interface {
+	area() float64
+}
+
+func escreverArea(f forma) {
+	fmt.Printf("A área da forma é %0.2f\n", f.area())
+}
+
+
+
+func main() {
+	fmt.Println("Aula 24 - Interfaces")
+
+	r := retangulo{altura: 10, largura: 15}
+	escreverArea(r)
+}
